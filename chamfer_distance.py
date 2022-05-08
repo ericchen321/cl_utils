d783dc4c65fceafdc3567462f0ff1f52a245c467
eric <cgx1997@outlook.com>
2022-05-08 12:36:31 -0700

    moved point sampling to after normalization

--- chamfer_distance.py
+++ chamfer_distance.py
@@ -9,14 +9,8 @@ from utils import scale_to_unit_sphere
 import argparse
 
 
-def sample_points_from_shape(shape_in_path, num_pts):
-    mesh = trimesh.load(shape_in_path)
-    mesh = scale_to_unit_sphere(mesh)
-
-    num_verts = mesh.vertices.shape[0]
-    num_pts = min(num_pts, num_verts)
-    print(f"sampling {num_pts} points from shape {shape_in_path}")
-    samples = mesh.sample(num_pts) # Sample a subset of points for computation
+def sample_points_from_shape(mesh, num_pts):
+    samples = mesh.sample(num_pts) # Sample points (by interpolation) for computation
     return samples, mesh
 
 
@@ -33,23 +27,33 @@ if __name__ == '__main__':
     parser.add_argument(
         '--metric', type=str, default='l2')
     parser.add_argument(
-        '--num_pts', type=int, default=1000000
+        '--num_pts', type=int, default=1000000, help='number of points to sample for computing CD'
     )
     parser.add_argument(
         '--square_dist', action='store_true', default=False, help='square distance or not'
     )
     args = parser.parse_args()
 
+    pred_mesh, pred_samples, gt_mesh, gt_samples = None, None, None, None
     if args.pred_shape_input != '':
-        pred_samples, pred_mesh = sample_points_from_shape(args.pred_shape_input, args.num_pts)
+        # Load the mesh
+        pred_mesh = trimesh.load(args.pred_shape_input)
+        # Normalize
+        pred_mesh = scale_to_unit_sphere(pred_mesh)
+        # Output normalized shape
+        if args.pred_shape_norm_output !='':
+            pred_mesh.export(args.pred_shape_norm_output)
+        # Sample
+        pred_samples, pred_mesh = sample_points_from_shape(pred_mesh, args.num_pts)
     if args.gt_shape_input != '':
-        gt_samples, gt_mesh = sample_points_from_shape(args.gt_shape_input, args.num_pts)
-
-    # Output normalized gt && output shapes
-    if args.pred_shape_norm_output !='':
-        pred_mesh.export(args.pred_shape_norm_output)
-    if args.gt_shape_norm_output != '':
-        gt_mesh.export(args.gt_shape_norm_output)
+        # Load the mesh
+        gt_mesh = trimesh.load(args.gt_shape_input)
+        # Normalize
+        gt_mesh = scale_to_unit_sphere(gt_mesh)
+        if args.gt_shape_norm_output != '':
+            gt_mesh.export(args.gt_shape_norm_output)
+        # Sample
+        gt_samples, gt_mesh = sample_points_from_shape(gt_mesh, args.num_pts)
 
     # Compute Chamfer
     if (args.pred_shape_input != '' and args.gt_shape_input != ''):
