f622433ad81b4950bc7210097b6626fef78a3f58
eric <cgx1997@outlook.com>
2022-05-10 15:03:52 -0700

    allow single mesh input/output

--- normalize_mesh.py
+++ normalize_mesh.py
@@ -13,14 +13,23 @@ if __name__ == '__main__':
     parser.add_argument(
         '--in_dir', type=str, default='')
     parser.add_argument(
-    '--out_dir', type=str, default='')
+        '--out_dir', type=str, default='')
+    parser.add_argument(
+        '--in_mesh', type=str, default='')
+    parser.add_argument(
+        '--out_mesh', type=str, default='')
     args = parser.parse_args()
 
-    for filename in os.listdir(f"{args.in_dir}/"):
-        if filename.endswith((".obj", ".ply")):
-            print(f"Normalizing {args.in_dir}/{filename}, output to {args.out_dir}/{filename}")
-            mesh = trimesh.load(f"{args.in_dir}/{filename}")
-            unit_mesh = scale_to_unit_sphere(mesh)
-            unit_mesh.export(f"{args.out_dir}/{filename}")
-        else:
-            continue
+    if args.in_dir != "" and args.out_dir != "":
+        for filename in os.listdir(f"{args.in_dir}/"):
+            if filename.endswith((".obj", ".ply")):
+                print(f"Normalizing {args.in_dir}/{filename}, output to {args.out_dir}/{filename}")
+                mesh = trimesh.load(f"{args.in_dir}/{filename}")
+                unit_mesh = scale_to_unit_sphere(mesh)
+                unit_mesh.export(f"{args.out_dir}/{filename}")
+            else:
+                continue
+    elif args.in_mesh != "" and args.out_mesh != "":
+        mesh = trimesh.load(args.in_mesh)
+        unit_mesh = scale_to_unit_sphere(mesh)
+        unit_mesh.export(args.out_mesh)
