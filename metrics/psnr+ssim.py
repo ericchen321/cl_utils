4cbfa1b448393f30fc08deef038d48f56476f086
eric <cgx1997@outlook.com>
2022-05-19 00:56:29 -0700

    more robust handling of RGB/grayscale: require explicit setup in config

--- metrics/psnr+ssim.py
+++ metrics/psnr+ssim.py
@@ -8,6 +8,7 @@ import csv
 import yaml
 from PIL import Image
 import numpy as np
+from skimage import io
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
 
@@ -25,22 +26,25 @@ if __name__=='__main__':
 
     img_path_gt = config["img_paths"][0]
     im_gt_arr = np.array(Image.open(img_path_gt))
+    if config["grayscale"] and len(im_gt_arr.shape) == 3:
+        im_gt_arr = im_gt_arr[:, :, 0]
 
     result_filename = f"metrics/results/psnr+ssim_{config['experiment_name']}.csv"
     with open(result_filename, 'w', newline='') as result_file:
         resultwriter = csv.writer(result_file, delimiter=',')
         for img_path in config["img_paths"][1:]:
             im_pred_arr = np.array(Image.open(img_path))
-            if len(im_pred_arr.shape) == 3 and len(im_gt_arr.shape) == 2:
-                # handle the particular case where the GT image contains one channel
-                # but the predicted contains three
-                im_pred_arr = im_pred_arr[:, :, 0]
+            
+            if config["grayscale"]:
+                if len(im_pred_arr.shape) == 3:
+                    im_pred_arr = im_pred_arr[:, :, 0]
                 psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
-            else:
-                psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
-            if len(im_pred_arr.shape) == 3:
-                ssim = structural_similarity(im_gt_arr, im_pred_arr, multichannel=True)
-            else:
                 ssim = structural_similarity(im_gt_arr, im_pred_arr, multichannel=False)
+            else:
+                im_gt_arr = im_gt_arr[:, :, :3]
+                im_pred_arr = im_pred_arr[:, :, :3]
+                psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
+                ssim = structural_similarity(im_gt_arr, im_pred_arr, multichannel=True)
+            
             resultwriter.writerow([img_path, f"{psnr}", f"{ssim}"])
     print(f"PSNR and SSIM written to {result_filename}")
