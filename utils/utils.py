97e865cd52a58dbb1c00ce2ad9d221759b09fddb
eric <cgx1997@outlook.com>
2022-05-23 21:18:21 -0700

    removed .xyz file support for normalizing function

--- utils/utils.py
+++ utils/utils.py
@@ -8,8 +8,4 @@ def scale_to_unit_sphere(mesh):
     vertices = mesh.vertices - mesh.bounding_box.centroid
     distances = np.linalg.norm(vertices, axis=1)
     vertices /= np.max(distances)
-    if isinstance(mesh, trimesh.PointCloud):
-        # if the shape is a point cloud, put in vertices only
-        return trimesh.PointCloud(vertices=vertices)
-    else:
-        return trimesh.Trimesh(vertices=vertices, faces=mesh.faces)
+    return trimesh.Trimesh(vertices=vertices, faces=mesh.faces)
