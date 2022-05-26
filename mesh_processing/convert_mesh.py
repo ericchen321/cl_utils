3430cab7b168d4050ad09d58a65dbbe95e465e8a
eric <cgx1997@outlook.com>
2022-05-25 19:08:33 -0700

    allow converting a single mesh

--- mesh_processing/convert_mesh.py
+++ mesh_processing/convert_mesh.py
@@ -24,12 +24,20 @@ if __name__ == '__main__':
     parser.add_argument(
         '--in_dir', type=str, default='')
     parser.add_argument(
-    '--out_dir', type=str, default='')
+        '--out_dir', type=str, default='')
+    parser.add_argument(
+        '--in_mesh', type=str, default='')
+    parser.add_argument(
+        '--out_mesh', type=str, default='')
     args = parser.parse_args()
 
-    for filename_src in os.listdir(f"{args.in_dir}/"):
-        if filename_src.endswith((".obj", ".ply")):
-            filename_dst = get_converted_filename(filename_src)
-            print(f"Converting {args.in_dir}/{filename_src}, output to {args.out_dir}/{filename_dst}")
-            mesh = trimesh.load(f"{args.in_dir}/{filename_src}")
-            mesh.export(f"{args.out_dir}/{filename_dst}")
+    if args.in_dir != "" and args.out_dir != "":
+        for filename_src in os.listdir(f"{args.in_dir}/"):
+            if filename_src.endswith((".obj", ".ply")):
+                filename_dst = get_converted_filename(filename_src)
+                print(f"Converting {args.in_dir}/{filename_src}, output to {args.out_dir}/{filename_dst}")
+                mesh = trimesh.load(f"{args.in_dir}/{filename_src}")
+                mesh.export(f"{args.out_dir}/{filename_dst}")
+    elif args.in_mesh != "" and args.out_mesh != "":
+        mesh = trimesh.load(args.in_mesh)
+        mesh.export(args.out_mesh)
