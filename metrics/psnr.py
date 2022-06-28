82d20cc7376ab9a77f4967d39e2d0870f1959dde
eric <cgx1997@outlook.com>
2022-06-28 12:41:31 -0700

    psnr script can now batch-process multiple scenes

--- metrics/psnr.py
+++ metrics/psnr.py
@@ -13,23 +13,29 @@ from skimage.metrics import peak_signal_noise_ratio
 
 
 def compute_psnrs(config):
-    img_path_gt = config["img_paths"][0]
-    im_gt_arr = np.array(Image.open(img_path_gt))
-    if config["grayscale"] and len(im_gt_arr.shape) == 3:
-        im_gt_arr = im_gt_arr[:, :, 0]
+    img_paths_gt = config["gt_img_paths"]
+    img_paths_pred = config["pred_img_paths"]
+    grayscales = config["grayscales"]
+
     psnrs = []
-    for img_path in config["img_paths"][1:]:
-            im_pred_arr = np.array(Image.open(img_path))
-            psnr = None
-            if config["grayscale"]:
-                if len(im_pred_arr.shape) == 3:
-                    im_pred_arr = im_pred_arr[:, :, 0]
-                psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
-            else:
-                im_gt_arr = im_gt_arr[:, :, :3]
-                im_pred_arr = im_pred_arr[:, :, :3]
-                psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
-            psnrs.append(psnr)
+    for img_path_gt, img_path_pred, is_gs in zip(
+        img_paths_gt, img_paths_pred, grayscales):
+        # for each gt-pred pair, compute psnr
+        im_gt_arr = np.array(Image.open(img_path_gt))
+        im_pred_arr = np.array(Image.open(img_path_pred))
+        psnr = None
+        if is_gs:
+            if len(im_gt_arr.shape) == 3:
+                im_gt_arr = im_gt_arr[:, :, 0]
+            if len(im_pred_arr.shape) == 3:
+                im_pred_arr = im_pred_arr[:, :, 0]
+            psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
+        else:
+            im_gt_arr = im_gt_arr[:, :, :3]
+            im_pred_arr = im_pred_arr[:, :, :3]
+            psnr = peak_signal_noise_ratio(im_gt_arr, im_pred_arr)
+        psnrs.append(psnr)
+    
     return psnrs
 
 
@@ -51,6 +57,6 @@ if __name__=='__main__':
     with open(result_filename, 'w', newline='') as result_file:
         resultwriter = csv.writer(result_file, delimiter=',')
         for imgpath_pred, psnr in zip(
-            config["img_paths"][1:], psnrs):
+            config["pred_img_paths"], psnrs):
             resultwriter.writerow([imgpath_pred, psnr])
     print(f"PSNR written to {result_filename}")
